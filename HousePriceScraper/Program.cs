3a94bff897dc5917c5f350131cea772834a4e231
Zhenyu Shi <errisy@gmail.com>
2018-05-20 13:00:50 +1000

    setup

---
+++ HousePriceScraper/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using OpenQA.Selenium.Chrome;
+
+
+namespace HousePriceScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            using(ChromeDriver chromeDriver = new ChromeDriver())
+            {
+                chromeDriver.Url = "https://www.realestate.com.au/buy";
+                chromeDriver.Navigate();
+
+                var searchInputElement = chromeDriver.FindElementByCssSelector("input.rui-input.rui-location-box.rui-auto-complete-input");
+
+                searchInputElement.SendKeys("Sunnybank, QLD 4109; ");
+
+                var searchButtonElement = chromeDriver.FindElementByCssSelector("button.rui-search-button");
+
+                searchButtonElement.Click();
+
+                var mapLinkElement = chromeDriver.FindElementById("mapLink");
+
+                mapLinkElement.Click();
+
+                Debugger.Break();
+            }
+        }
+    }
+}
