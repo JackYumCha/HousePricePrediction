46b4042c3fa5a52d6f42451b2bc7ff1a8b0f23be
Zhenyu Shi <errisy@gmail.com>
2018-05-20 17:17:57 +1000

    allow run from last position

--- HousePriceScraper/Program.cs
+++ HousePriceScraper/Program.cs
@@ -23,6 +23,16 @@ namespace HousePriceScraper
 
             Spider spider = new Spider();
 
+            int lineNumber = 0;
+            string lineNumberPath = $"{AppContext.BaseDirectory}/line.txt";
+
+            if (File.Exists(lineNumberPath))
+            {
+                lineNumber = int.Parse(File.ReadAllText(lineNumberPath));
+            }
+
+            int index = 0;
+
             using (StreamReader csvStreamReader = new StreamReader(pathCsv))
             {
                 using(CsvReader csvReader = new CsvReader(csvStreamReader))
@@ -32,27 +42,39 @@ namespace HousePriceScraper
 
                     while (csvReader.Read())
                     {
-                        var row = csvReader.GetRecord<dynamic>();
-                        var dict = row as IDictionary<string, object>;
+                        if(index < lineNumber)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            var row = csvReader.GetRecord<dynamic>();
+                            var dict = row as IDictionary<string, object>;
 
-                        var prop = new Property();
+                            var prop = new Property();
 
-                        prop.UnitNumber = dict["UNIT NUMBER"] as string;
-                        prop.HouseNumber = dict["HOUSE NUMBER"] as string;
-                        prop.StreetName = dict["STREET NAME"] as string;
-                        prop.StreetType = dict["STREET TYPE"] as string;
-                        prop.StreetSuffix = dict["STREET SUFFIX"] as string;
-                        prop.Suburb = dict["SUBURB"] as string;
-                        prop.Postcode = dict["POSTCODE"] as string;
-                        prop.AddressUseType = dict["ADDRESS USE TYPE"] as string;
-                        prop.WardName = dict["WARD NAME"] as string;
-                        prop.PropertyDescription = dict["PROPERTY DESCRIPTION"] as string;
+                            prop.UnitNumber = dict["UNIT NUMBER"] as string;
+                            prop.HouseNumber = dict["HOUSE NUMBER"] as string;
+                            prop.StreetName = dict["STREET NAME"] as string;
+                            prop.StreetType = dict["STREET TYPE"] as string;
+                            prop.StreetSuffix = dict["STREET SUFFIX"] as string;
+                            prop.Suburb = dict["SUBURB"] as string;
+                            prop.Postcode = dict["POSTCODE"] as string;
+                            prop.AddressUseType = dict["ADDRESS USE TYPE"] as string;
+                            prop.WardName = dict["WARD NAME"] as string;
+                            prop.PropertyDescription = dict["PROPERTY DESCRIPTION"] as string;
 
-                        prop.BuildKey();
+                            prop.BuildKey();
 
-                        spider.Search(prop);
+                            spider.Search(prop);
+
+                            File.WriteAllText(lineNumberPath, index.ToString());
+                        }
+                     
                     }
                 }
+
+
             }
 
         }
