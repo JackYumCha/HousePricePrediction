5e1b1ccc8b201ec4f7dbc63798f03ec78d99507a
BoowenLi <37534244+BoowenLi@users.noreply.github.com>
2018-05-20 15:41:26 +1000

    as
    s

--- HousePriceScraper/HousePriceScraper/Program.cs
+++ HousePriceScraper/HousePriceScraper/Program.cs
@@ -3,6 +3,9 @@ using OpenQA.Selenium.Chrome;
 using System.Diagnostics;
 using OpenQA.Selenium;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
 
 
 namespace HousePriceScraper
@@ -11,35 +14,13 @@ namespace HousePriceScraper
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("--headless");
-            options.AddArgument("--disable-gpu");
-            options.AddArgument("--no-sandbox");
-            options.AddArgument("--log-level=3");
-
-
-            using (ChromeDriver chromeDriver = new ChromeDriver(options))
+            Regex csv = new Regex(@"~\-\-csv=");
+            var argCsvPath = args.FirstOrDefault(arg => csv.IsMatch(arg));
+            var pathCsv = csv.Replace(argCsvPath, "");
+            using (StreamReader csvStreamReader = new StreamReader(pathCsv))
             {
+                using(CsvReader csvReader = new CsvReader())
 
-                chromeDriver.Url = "https://www.realestate.com.au/buy";
-                chromeDriver.Navigate();
-
-                var searchInputElement = chromeDriver.FindElementByCssSelector("input.rui-input.rui-location-box.rui-auto-complete-input");
-                searchInputElement.SendKeys("Sunnybank, QLD 4109");
-                var searchBottonElement = chromeDriver.FindElementByCssSelector("button.rui-search-button");
-                searchBottonElement.Click();
-
-                var advertisements = chromeDriver.FindElementsByCssSelector("div.listingInfo.rui-clearfix");
-                List<string> addresses = new List<string>();
-                foreach(var ad in advertisements)
-                {
-                    var addressElement = ad.FindElement(By.CssSelector("div.vcard"));
-                    addresses.Add(addressElement.Text);
-                }
-                //var mapLinkElement = chromeDriver.FindElementById("mapLink");
-                //mapLinkElement.Click();
-                Debugger.Break(); 
 
             }
 
