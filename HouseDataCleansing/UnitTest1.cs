f3755b4205667a7d9c4bd588596892942700f85b
Zhenyu Shi <errisy@gmail.com>
2018-05-20 18:17:49 +1000

    last change

--- HouseDataCleansing/UnitTest1.cs
+++ HouseDataCleansing/UnitTest1.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using Xunit;
 using Newtonsoft.Json;
+using CsvHelper;
 
 namespace HouseDataCleansing
 {
@@ -13,17 +14,82 @@ namespace HouseDataCleansing
 
             var fileList = Directory.GetFiles(@"C:\Users\erris\Desktop\House Data\4110");
 
-            foreach(string filename in fileList)
+            var resultCsv = @"C:\Users\erris\Desktop\House Data\4110.csv";
+
+            var resultHistoryRoot = @"C:\Users\erris\Desktop\House Data\4110History";
+
+            using(StreamWriter sw = new StreamWriter(resultCsv))
             {
-                string json =  File.ReadAllText(filename);
-                var property = JsonConvert.DeserializeObject<HousePriceScraper.Property>(json);
+                using(CsvWriter csv = new CsvWriter(sw))
+                {
+                    csv.WriteField("Unit");
+                    csv.WriteField("StreetNumber");
+                    csv.WriteField("StreetName");
+                    csv.WriteField("StreetType");
+                    csv.WriteField("StreetSuffix");
+                    csv.WriteField("NumberOfBedrooms");
+                    csv.WriteField("NumberOfBathrooms");
+                    csv.WriteField("NumberOfParkings");
+                    csv.WriteField("MiddlePrice");
+                    csv.NextRecord();
 
-                // number of rooms, number of parking, number of bathrooms
-                // year build, landsize
+                    foreach (string filename in fileList)
+                    {
+                        string json = File.ReadAllText(filename);
+                        var property = JsonConvert.DeserializeObject<HousePriceScraper.Property>(json);
 
-                 
+                        // number of rooms, number of parking, number of bathrooms
+                        // year build, landsize
 
+                        int? numberOfBedrooms = property.DomainBedroom.TryParseInt();
+
+                        int? numberOfBathrooms = property.DomainBathroom.TryParseInt();
+
+                        int? numberOfParkings = property.DomainParking.TryParseInt();
+
+                        double? middle = property.DomainMidValue.TryParsePrice();
+
+                        csv.WriteField(property.UnitNumber);
+                        csv.WriteField(property.HouseNumber);
+                        csv.WriteField(property.StreetName);
+                        csv.WriteField(property.StreetType);
+                        csv.WriteField(property.StreetSuffix);
+                        csv.WriteField(numberOfBedrooms);
+                        csv.WriteField(numberOfBathrooms);
+                        csv.WriteField(numberOfParkings);
+                        csv.WriteField(middle);
+                        csv.NextRecord();
+
+                        if(property.DomainRecords != null && property.DomainRecords.Count > 0)
+                        {
+
+                            using (StreamWriter swHis = new StreamWriter($@"{resultHistoryRoot}\{property._key}.csv"))
+                            {
+                                using (CsvWriter csvHis = new CsvWriter(swHis))
+                                {
+
+                                    csvHis.WriteField("Action");
+                                    csvHis.WriteField("Agent");
+                                    csvHis.WriteField("Date");
+                                    csvHis.WriteField("Value");
+                                    csvHis.NextRecord();
+
+                                    foreach (var record in property.DomainRecords)
+                                    {
+                                        csvHis.WriteField(record.Action);
+                                        csvHis.WriteField(record.Agent);
+                                        csvHis.WriteField(record.Date);
+                                        csvHis.WriteField(record.Value);
+                                        csvHis.NextRecord();
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
             }
+            
 
         }
 
