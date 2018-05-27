1ceb8943456f39f9f198e055bca249889027dc11
Zhenyu Shi <errisy@gmail.com>
2018-05-27 16:03:54 +1000

    program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using MvcAngular.Generator;
 
 namespace API
 {
@@ -14,12 +8,17 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            if (AngularGenerator.ShouldRunMvc(args))
+            {
+                BuildWebHost(args).Run();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://*:80")
                 .Build();
     }
+
 }
