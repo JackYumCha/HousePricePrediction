124ebe029bb9e5450d90389ec8308d22d7a981ea
JacobXing <37539965+JacobXing@users.noreply.github.com>
2018-07-12 17:11:22 +1000

    dsa
    dsadsad

---
+++ ui/src/app/services/mvc-api/datatypes/API.Entities.HousePredictionRequest.ts
@@ -0,0 +1,15 @@
+﻿/** 
+ * Auto Generated Code
+ * Please do not modify this file manually 
+ * Assembly Name: "API"
+ * Source Type: "C:\Users\13661\OneDrive\文档\GitHub\HousePricePrediction\API\bin\Debug\netcoreapp2.0\API.dll"
+ * Generated At: 2018-05-27 18:46:58.827
+ */
+
+export interface HousePredictionRequest {
+	NumberOfBedrooms?: number;
+	NumberOfBathrooms?: number;
+	NumberOfParkings?: number;
+	Latitude?: number;
+	Longitude?: number;
+}
---
+++ ui/src/app/services/mvc-api/services/API.Controllers.Prediction.Service.ts
@@ -0,0 +1,20 @@
+﻿/** 
+ * Auto Generated Code
+ * Please do not modify this file manually 
+ * Assembly Name: "API"
+ * Source Type: "C:\Users\13661\OneDrive\文档\GitHub\HousePricePrediction\API\bin\Debug\netcoreapp2.0\API.dll"
+ * Generated At: 2018-05-27 18:46:58.847
+ */
+import { HousePredictionRequest } from '../datatypes/API.Entities.HousePredictionRequest';
+import { HousePredictionResponse } from '../datatypes/API.Entities.HousePredictionResponse';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Injectable()
+export class PredictionService {
+	constructor(private $httpClient: HttpClient) {{}}
+	public $baseURL: string = '<API>';
+	public Predict(request: HousePredictionRequest): Observable<HousePredictionResponse> {
+		return this.$httpClient.post<HousePredictionResponse>(this.$baseURL + 'Prediction/Predict', request, {});
+	}
+}
